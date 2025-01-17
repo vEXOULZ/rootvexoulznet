722138bb44ec528ead745939035c7e8f920cc76a
vEXOULZ <vexoulz@gmail.com>
2025-01-16 23:35:56 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<script setup>
+import SocialLink from './components/SocialLink.vue'
+</script>
+
+<template>
+
+    <intro>
+      <img alt="logo" class="logo" src="./assets/vexoulHELP.png" width="213" height="75" />
+      <p>Did you nose?</p>
+      <p>Vexoulz is not real</p>
+      <p>You are not real</p>
+      <p>No one is real</p>
+      <p>The only thing that's real</p>
+      <p>Is my love for yo momma</p>
+    </intro>
+
+
+  <main>
+      <SocialLink msg="Bluesky"    img="src/assets/log_bluesky.png" submsg="@vexoulz.net"     link="https://bsky.app/profile/vexoulz.net" style="color: rgb(16, 129, 246);"/>
+      <SocialLink msg="Twitch"     img="src/assets/log_twitch.png"  submsg="ttv/vexoulz"      link="https://twitch.tv/vexoulz"            style="color: rgb(172, 81, 248);"/>
+      <SocialLink msg="Vods"       img="src/assets/log_vods.png"    submsg="vods.vexoulz.net" link="https://vods.vexoulz.net"             style="color: rgb(224, 179, 157);"/>
+      <SocialLink msg="Merch shop" img="src/assets/log_shop.png"    submsg="shop.vexoulz.net" link="https:/shop.vexoulz.net"              style="color: rgb(200, 200, 200);"/>
+  </main>
+
+  <footer>
+    Made by vEXOULZ @ 2025 with pain. HELP
+  </footer>
+
+</template>
+
+<style scoped>
+body {
+  min-height: 100vh;
+  margin: 0;
+  display: grid;
+  grid-template-rows: auto 1fr auto;
+} 
+
+intro {
+  line-height: 1.5;
+  min-height: 50px;
+}
+
+footer {
+  min-height: 50px;
+  place-items: center;
+  padding-left: calc(var(--section-gap));
+}
+
+@media (min-width: 128px) {
+  intro {
+    display: grid;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 4);
+  }
+
+  intro .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/SocialLink.vue
@@ -0,0 +1,128 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true,
+  },
+  img: {
+    type: String,
+    required: false,
+  },
+  submsg: {
+    type: String,
+    required: false,
+  },
+  link: {
+    type: String,
+    required: false,
+  },
+  style: {
+    type: String,
+    required: false,
+  }
+})
+</script>
+
+<template>
+  <div class="item">
+    <i>
+      <a :href="link" target="_blank" rel="noopener">
+        <img :alt="msg" class="logo" :src="img" width="35" height="35" />
+      </a>
+    </i>
+    <a :href="link" target="_blank" rel="noopener">
+      <div class="details">
+          <h3>
+            {{ msg }}
+          </h3>
+          <p class="submsg" :style="style">{{ submsg }}</p>
+      </div>
+    </a>
+  </div>
+</template>
+
+<style scoped>
+
+.submsg {
+  opacity: 0.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 0rem;
+}
+
+.item {
+  margin-top: 0rem;
+  display: flex;
+  position: relative;
+}
+
+.details {
+  flex: 1;
+  margin-left: 0rem;
+}
+
+i {
+  display: flex;
+  place-items: center;
+  place-content: center;
+  width: 50px;
+  height: 50px;
+
+  color: var(--color-text);
+}
+
+h3 {
+  font-size: 1.2rem;
+  font-weight: 500;
+  padding-top: 0rem;
+  margin-top: -0.2rem;
+  margin-bottom: 0rem;
+  color: var(--color-heading);
+}
+
+@media (min-width: 128px) {
+  .item {
+    margin-top: 0;
+    padding: 0.1rem 0 0.5rem calc(var(--section-gap) / 5);
+  }
+
+  i {
+    top: calc(50% - 25px);
+    left: -26px;
+    position: absolute;
+    border: 1px solid var(--color-border);
+    background: var(--color-background);
+    border-radius: 8px;
+    width: 50px;
+    height: 50px;
+  }
+
+  .item:before {
+    content: ' ';
+    border-left: 1px solid var(--color-border);
+    position: absolute;
+    left: 0;
+    bottom: calc(50% + 25px);
+    height: calc(50% - 25px);
+  }
+
+  .item:after {
+    content: ' ';
+    border-left: 1px solid var(--color-border);
+    position: absolute;
+    left: 0;
+    top: calc(50% + 25px);
+    height: calc(50% - 25px);
+  }
+
+  .item:first-of-type:before {
+    display: none;
+  }
+
+  .item:last-of-type:after {
+    display: none;
+  }
+}
+</style>
