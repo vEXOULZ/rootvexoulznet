f488b33b353199f5c0e095537211b92dc81f85f3
vEXOULZ <vexoulz@gmail.com>
2025-07-24 18:45:09 -0300

    swap socials and stream columns

--- src/Landing.vue
+++ src/Landing.vue
@@ -39,8 +39,21 @@ import log_youtube from './assets/log_youtube.png'
         <p>Is my love for yo momma</p>
         </div>
 
-        <div style="grid-row: 1 / span 2; grid-column: 2;">
-          <ItemGroup title="Socials" style="grid-row: 1; grid-column: 2;">
+        <ItemGroup title="Stream" style="grid-row: 1; grid-column: 2;">
+          <ItemLink msg="Twitch"     :img=log_twitch  submsg="ttv/vexoulz"      link="https://twitch.tv/vexoulz"             style="color: rgb(172, 81, 248);"/>
+          <ItemLink msg="Vods"       :img=log_vods    submsg="vods.vexoulz.net" link="https://vods.vexoulz.net"              style="color: rgb(222, 114, 25);"/>
+          <ItemLink msg="Tiktok"     :img=log_tiktok  submsg="tiktok/@vexoulz"  link="https://tiktok.com/@vexoulz"           style="color: rgb(100, 200, 200);"/>
+          <ItemLink msg="Youtube"    :img=log_youtube submsg="yt/@vexoulz"      link="https://youtube.com/@vEXOULZ"          style="color: rgb(200, 0, 0);"/>
+          <!-- <ItemLink msg="OBS Sources":img=log_obs     submsg="./obs_sources"    link="/obs_sources"                          style="color: rgb(200, 200, 200);"/> -->
+        </ItemGroup>
+
+        <ItemGroup title="Dev" style="grid-row: 2; grid-column: 2;">
+          <ItemLink msg="Github"     :img=log_github  submsg="github/vEXOULZ"   link="https://github.com/vEXOULZ"            style="color: rgb(120, 120, 120);"/>
+          <!-- <ItemLink msg="DTP bot"    :img=log_dtp     submsg="dtp.vexoulz.net"  link="https://dtp.vexoulz.net"               style="color: rgb(182, 163, 108);"/> -->
+        </ItemGroup>
+
+        <div style="grid-row: 1 / span 2; grid-column: 3;">
+          <ItemGroup title="Socials">
             <ItemLink msg="Bluesky"    :img=log_bluesky submsg="@vexoulz.net"     link="https://bsky.app/profile/vexoulz.net"  style="color: rgb(16, 129, 246);"/>
             <ItemLink msg="Discord"    :img=log_discord submsg="vEXcord server"   link="https://discord.gg/vexoulz"            style="color: rgb(86, 98, 246);"/>
             <ItemLink msg="Steam"      :img=log_steam   submsg="steam/vexoulz"    link="https://steamcommunity.com/id/vexoulz/"style="color: rgb(120, 120, 120);"/>
@@ -52,18 +65,6 @@ import log_youtube from './assets/log_youtube.png'
           </ItemGroup>
         </div>
 
-        <ItemGroup title="Stream" style="grid-row: 1; grid-column: 3;">
-          <ItemLink msg="Twitch"     :img=log_twitch  submsg="ttv/vexoulz"      link="https://twitch.tv/vexoulz"             style="color: rgb(172, 81, 248);"/>
-          <ItemLink msg="Vods"       :img=log_vods    submsg="vods.vexoulz.net" link="https://vods.vexoulz.net"              style="color: rgb(222, 114, 25);"/>
-          <ItemLink msg="Tiktok"     :img=log_tiktok  submsg="tiktok/@vexoulz"  link="https://tiktok.com/@vexoulz"           style="color: rgb(100, 200, 200);"/>
-          <ItemLink msg="Youtube"    :img=log_youtube submsg="yt/@vexoulz"      link="https://youtube.com/@vEXOULZ"          style="color: rgb(200, 0, 0);"/>
-          <!-- <ItemLink msg="OBS Sources":img=log_obs     submsg="./obs_sources"    link="/obs_sources"                          style="color: rgb(200, 200, 200);"/> -->
-        </ItemGroup>
-
-        <ItemGroup title="Dev" style="grid-row: 2; grid-column: 3;">
-          <ItemLink msg="Github"     :img=log_github  submsg="github/vEXOULZ"   link="https://github.com/vEXOULZ"            style="color: rgb(120, 120, 120);"/>
-          <!-- <ItemLink msg="DTP bot"    :img=log_dtp     submsg="dtp.vexoulz.net"  link="https://dtp.vexoulz.net"               style="color: rgb(182, 163, 108);"/> -->
-        </ItemGroup>
 
     </div>
   </StandardPage>
